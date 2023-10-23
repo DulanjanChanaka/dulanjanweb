6c1141fa8ab024f3bd4d7cc193c51302d8d1d3bb
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-10-24 00:52:18 +0530

    footer change

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import About from '@/components/About'
 import Blog from '@/components/Blog'
 import Contact from '@/components/Contact'
+import Footer from '@/components/Footer'
 import Main from '@/components/Main'
 import Navbar from '@/components/Navbar'
 import Projects from '@/components/Projects'
 import Skills from '@/components/Skills'
-import Footer from '@/components/footer'
+
 import Image from 'next/image'
 
 export default function Home() {
